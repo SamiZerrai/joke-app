300dd96084a35417d1764270487acdbff0aaed11
ZERRAI Sami <sami.zerrai@gmail.com>
2022-09-22 08:14:23 +0200

    add exo

---
+++ py.py
@@ -0,0 +1,14 @@
+# On vous fournit un fichier notes.json représentant les notes des élèves dans les 3 matières maths, physique et histoire.
+
+# Le format json est le suivant :
+
+# { students: [ { name:"XXX",noteMaths:1,notePhysique:4,noteHistoire:2}, ...{ name:"ZZZ",noteMaths:1,notePhysique:4,noteHistoire:2}]}
+# Après avoir chargé le fichier, affichez chaque élève (un par ligne) avec sa moyenne, au format "nom : moyenne". La moyenne sera arrondie a une décimale.
+
+# Exemple, si le fichier contient :
+# {students:[{name:"Ben", noteMaths:10, notePhysique:15, noteHistoire:20},
+# {name:"Sophie", noteMaths:12,notePhysique:4, noteHistoire:18}]}
+
+# L'affichage sera :
+# Ben : 15.0
+# Sophie : 11.3
